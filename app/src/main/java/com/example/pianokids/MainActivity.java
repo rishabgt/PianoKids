bb438d9ac6bd5b2fbc4a54a05e46ef8f3ec94abf
roypriya <priyaroystcet@gmail.com>
2019-10-29 08:44:14 +0530

    All the letters added

--- app/src/main/java/com/example/pianokids/MainActivity.java
+++ app/src/main/java/com/example/pianokids/MainActivity.java
@@ -65,6 +65,17 @@ public class MainActivity extends AppCompatActivity {
         m6SoundId = mSoundPool.load(this,R.raw.p6,1);
         m7SoundId = mSoundPool.load(this,R.raw.p7,1);
         m9SoundId = mSoundPool.load(this,R.raw.p9,1);
+        mCSoundId = mSoundPool.load(this,R.raw.pc,1);
+        mGSoundId = mSoundPool.load(this,R.raw.pg,1);
+        mISoundId = mSoundPool.load(this,R.raw.pi,1);
+        mOSoundId = mSoundPool.load(this,R.raw.po,1);
+        mPSoundId = mSoundPool.load(this,R.raw.pp,1);
+        mTSoundId = mSoundPool.load(this,R.raw.pt,1);
+        mUSoundId = mSoundPool.load(this,R.raw.pu,1);
+        mVSoundId = mSoundPool.load(this,R.raw.pv,1);
+        mXSoundId = mSoundPool.load(this,R.raw.px,1);
+        mYSoundId = mSoundPool.load(this,R.raw.py,1);
+        mZSoundId = mSoundPool.load(this,R.raw.pz,1);
 
         editText = findViewById(R.id.editText);
     }
@@ -81,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
     public void playE(View view){mSoundPool.play(mESoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);}
     public void playR(View view){mSoundPool.play(mRSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);}
     public void play5(View view){mSoundPool.play(m5SoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);}
+
     public void play(View view)throws Exception{
         String s = String.valueOf(editText.getText());
         int len = s.length();
@@ -91,28 +103,76 @@ public class MainActivity extends AppCompatActivity {
                 case 'b':
                 case 'B':mSoundPool.play(mBSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                           break;
-
+                case 'h':
                 case 'H':mSoundPool.play(mHSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                           break;
+                case 'n':
                 case 'N':mSoundPool.play(mNSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                     break;
+                case 'q':
                 case 'Q':mSoundPool.play(mQSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                     break;
                 case '2':mSoundPool.play(m2SoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                     break;
+                case 'w':
                 case 'W':mSoundPool.play(mWSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                     break;
+                case 'j':
                 case 'J':mSoundPool.play(mJSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                     break;
+                case 'm':
                 case 'M':mSoundPool.play(mMSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                     break;
                 case '3':mSoundPool.play(m3SoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                     break;
+                case 'e':
                 case 'E':mSoundPool.play(mESoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                     break;
+                case 'r':
                 case 'R':mSoundPool.play(mRSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                     break;
                 case '5':mSoundPool.play(m5SoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case '0':mSoundPool.play(m0SoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case '6':mSoundPool.play(m6SoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case '7':mSoundPool.play(m7SoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case '9':mSoundPool.play(m9SoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 'c':
+                case 'C':mSoundPool.play(mCSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 'g':
+                case 'G':mSoundPool.play(mGSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 'i':
+                case 'I':mSoundPool.play(mISoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 'o':
+                case 'O':mSoundPool.play(mOSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 'p':
+                case 'P':mSoundPool.play(mPSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 't':
+                case 'T':mSoundPool.play(mTSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 'u':
+                case 'U':mSoundPool.play(mUSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 'v':
+                case 'V':mSoundPool.play(mVSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 'x':
+                case 'X':mSoundPool.play(mXSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 'y':
+                case 'Y':mSoundPool.play(mYSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
+                    break;
+                case 'z':
+                case 'Z':mSoundPool.play(mZSoundId,LEFT_VOLUME,RIGHT_VOLUME,0,0,1);
                     break;
                 default:break;
             }
